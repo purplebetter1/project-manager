05a3963114c284afe3a18ef9058f266fc28eefeb
andrewgrablewski <agrablewski@gmail.com>
2022-06-25 16:21:35 -0400

    add client functionality

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 
 export default function AddClientModal() {
@@ -9,9 +11,26 @@ export default function AddClientModal() {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
 
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: {name, email, phone},
+    update(cache, {data: {addClient}}) {
+        const { clients } = cache.readQuery({ query: GET_CLIENTS});
+        cache.writeQuery({
+            query: GET_CLIENTS,
+            data: { clients: clients.concat([addClient])},
+        });
+    }
+  })
+
   const onSubmit= (e) => {
     e.preventDefault();
-    console.log("Form Submitted!", name, email, phone);
+    if(name === '' || phone === '' || email === '') {
+        return alert("Please fill in all fields");
+    }
+    addClient(name, email, phone);
+    setName('');
+    setEmail('');
+    setPhone('');
   }
 
   return (
