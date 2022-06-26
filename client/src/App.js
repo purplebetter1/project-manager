06e8fba7673e6df8abc030f9ed5437b18d3feb35
andrewgrablewski <agrablewski@gmail.com>
2022-06-26 11:03:15 -0400

    add home and not-found

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Header from './components/Header';
-import Clients from './components/Clients';
-import AddClientModal from './components/AddClientModal';
-import Projects from './components/Projects';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -33,12 +33,15 @@ function App() {
   return (
     <>
     <ApolloProvider client={client}>
-    <Header />
-    <div className="container">
-      <AddClientModal />
-      <Projects />
-      <Clients />
-    </div>
+      <Router>
+      <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider> 
     </>  
   );
