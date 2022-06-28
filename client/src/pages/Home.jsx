8b7689ab6c433a22f3608eec2a158d4bfcd1382a
andrewgrablewski <agrablewski@gmail.com>
2022-06-28 13:22:05 -0400

    add addProject modal

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Clients from '../components/Clients';
 import AddClientModal from '../components/AddClientModal';
+import AddProjectModal from '../components/AddProjectModal';
 import Projects from '../components/Projects';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
       </div>
       <Projects />
       <hr />
